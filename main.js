cd472bfb0afdc5f6b5dd5252e8546157d43dca60
Athar Laique <atharlaique15@gmail.com>
2021-06-10 12:21:43 +0500

    lession one

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-var name = 'Athar Laique';
+// -> Here we are creating instance of Vue object.
+// -> 'el' property reffers to the element in the dom.
+// -> 'data' property is reffer to data that we want to pass to our dom element.
+// -> To display data in dom elemnt we use {{}} that are called experession.
+// -> we can use expression for condtions as well.
+var app = new Vue({
+  el: '#app',
+  data: {
+    name: 'Athar Laique',
+    description: 'I ma a web developer',
+  },
+});
