9e87288704027d8d681b2d0d020009235fa59edc
Athar Laique <atharlaique15@gmail.com>
2021-06-11 15:37:23 +0500

    api call in component

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -5,3 +5,17 @@
         <router-link :to="{name:'EventShow', params:{ id: 1}}">Show</router-link>
     </div>
 </template>
+<script>
+    import EventService from '../services/apis/EventService';
+    export default {
+        created () {
+            EventService.getEvents()
+            .then((data)=> {
+                console.log(data)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+        }
+    }
+</script>
