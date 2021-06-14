5c211be07bd286d9bb5d6c3253d118ae678ae862
Athar Laique <atharlaique15@gmail.com>
2021-06-14 12:36:46 +0500

    simple counter app

---
+++ src/components/Counter.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="container">
+    <span>{{ countNow }}</span>
+    <button class="reset" @click="reset()">0</button>
+    <button class="plus" @click="add()">+</button>
+    <button class="minus" @click="subtract()">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    add() {
+      this.count += 1;
+    },
+    subtract() {
+      if (this.count > 0) {
+        this.count -= 1;
+      }
+    },
+    reset() {
+      this.count = 0;
+    },
+  },
+  computed: {
+    countNow() {
+      return this.count;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.container > span {
+  color: white;
+  font-size: 86px;
+  display: block;
+  font-weight: bold;
+}
+.container > button {
+  padding: 10px;
+  width: 100px;
+  margin: 0px 10px;
+  font-size: 22px;
+  border: none;
+  border-radius: 8px;
+  background-color: lightgray;
+  cursor: pointer;
+}
+.reset {
+  color: goldenrod;
+}
+.plus {
+  color: chartreuse;
+}
+.minus {
+  color: crimson;
+}
+</style>
