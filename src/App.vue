776b4621d53c3538798a2237246242531b1fc289
Athar Laique <atharlaique15@gmail.com>
2021-06-14 17:47:23 +0500

    project setup

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<style>
+html,
+body {
+  margin: 0 !important;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  background-color: #42b983;
+  height: 100vh;
+}
+</style>
